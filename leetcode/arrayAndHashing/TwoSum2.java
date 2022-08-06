74cf8a7b371563df162261df92bc6da11e27ecc7
Jagabandhu Malik <jagabandhu.malik@hm.com>
2022-08-06 12:07:55 +0530

    TwoSum II is implemented using two pointer

---
+++ leetcode/arrayAndHashing/TwoSum2.java
@@ -0,0 +1,26 @@
+package leetcode.arrayAndHashing;
+
+import java.util.Arrays;
+
+public class TwoSum2 {
+  public static  int[] twoSum(int[] numbers, int target) {
+    int leftPointer = 0;
+    int rightPointer = numbers.length-1;
+
+    while (leftPointer < rightPointer){
+      if (numbers[leftPointer] + numbers[rightPointer] == target){
+        return new int[]{leftPointer+1, rightPointer+1};
+      }
+      if(numbers[leftPointer] + numbers[rightPointer] > target){
+        rightPointer --;
+      }else if(numbers[leftPointer] + numbers[rightPointer] < target){
+       leftPointer++;
+      }
+    }
+
+    return new int[]{};
+  }
+  public static void main(String[] args){
+    System.out.println(Arrays.toString(twoSum(new int[] {2,7,11,15}, 9)) );
+  }
+}
