c6e13059ba71a3423922d38fca606dff0acaf890
Jagabandhu Malik <jagabandhu.malik@hm.com>
2022-08-06 10:56:26 +0530

    code format and TwoSum implemented

--- leetcode/arrayAndHashing/TwoSum.java
+++ leetcode/arrayAndHashing/TwoSum.java
@@ -1,5 +1,24 @@
 package leetcode.arrayAndHashing;
 
-public class TwoSum {
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
+public class TwoSum {
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++) {
+          if (map.get(target-nums[i]) != null){
+              return new int[] {map.get(target-nums[i]), i};
+          } else {
+              map.put(nums[i], i);
+          }
+        }
+        return new int[]{};
+    }
+
+    public static void main(String[] args) {
+        int[] res = twoSum(new int[]{2,7,11,15},9);
+        System.out.println(Arrays.toString(res));
+    }
 }
